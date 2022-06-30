5d65837ec49bd85cf6c4ec4c8af9e54e58c1ab40
Majdi Hajjem <hajjem.mejdi@gmail.com>
2022-06-30 18:36:09 +0100

    last commit

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
-var dt=Date.now()
-
+app.set('view engine', 'pug');
+app.set('views','./pages');
 const checkTime = (req, res, next) => {
-    console.log('Time:',dt);
-    next();
+    var d=new Date().getDay()
+    var h=new Date().getHours()
+    if((h >= 9 && h<18) && (d > 0 && d <6)){
+        next();
+    }else{
+        res.send("not found")
+    }
 }
 app.use(checkTime)
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/views/home.html')
+    // res.render('home');
 })
 app.get('/contact', (req, res)=>{
     res.sendFile(__dirname + '/views/Contactus.html')
